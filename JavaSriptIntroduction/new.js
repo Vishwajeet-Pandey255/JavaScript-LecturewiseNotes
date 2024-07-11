93a24b20c7ceafe7c684630d90280247897ee912
Vishwajeet Pandey <pandeyvishwajeet61@gmail.com>
2024-07-11 14:59:55 +0530

    js

---
+++ JavaSriptIntroduction/new.js
@@ -0,0 +1,43 @@
+//JavaScript is only support by web browser
+
+// console.log('helllow');  //For print the value like Syso
+
+// Datatype:-
+// Number -> int float etc
+// String
+// Boolean
+// undefined
+// null
+// BigInt
+// Symbol
+
+// int a = 5;  var a = 5  where var is a keyword in javaScript
+
+// var a=5;
+// var a='number'
+// var a=false
+// console.log(a);
+
+// var a=5 //Redeclaration is possible in var
+// var a=7
+
+
+// let b=10
+// let b=10   //Redeclaration is not possible  with same name
+// b=20    //reasingn value possible
+// console.log(b)
+
+
+// const c=10  //in const once we declare the value we can not reasign and redeclaction is not possible
+// c=193 
+// console.log(c);
+
+
+ let firstName='Vishwajeet'
+ let lastName='Pandey'
+ console.log('srk\nkhan')
+// console.log(`My firstName is: ${firstName}  My LastName is:  ${lastName}`);
+
+
+// let space=" "
+// console.log(firstName+space+lastName)
